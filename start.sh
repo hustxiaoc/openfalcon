2550db092b3bb9c3b0005f1cda9a4b683ec4cbe4
淘杰 <taojie.hjp@taobao.com>
2017-10-24 18:07:45 +0800

    fix: update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #/bin/bash
-cd  /dashboard 
-virtualenv ./env && ./env/bin/pip install -r pip_requirements.txt -i https://pypi.douban.com/simple
-./env/bin/python wsgi.py
 
-cd /tmp/falcon-plus/scripts/mysql/db_schema/
+cd /src/falcon-plus/scripts/mysql/db_schema/
 mysql -h 127.0.0.1 -u root -p < 1_uic-db-schema.sql
 mysql -h 127.0.0.1 -u root -p < 2_portal-db-schema.sql
 mysql -h 127.0.0.1 -u root -p < 3_dashboard-db-schema.sql
 mysql -h 127.0.0.1 -u root -p < 4_graph-db-schema.sql
 mysql -h 127.0.0.1 -u root -p < 5_alarms-db-schema.sql
 
-cd $WORKSPACE
+cd /src/open-falcon
 ./open-falcon start
+
+cd  /src/dashboard
+python wsgi.py
