5a71cf26bf4bf1ec107e4a9aca91ca33b14d17f3
淘杰 <taojie.hjp@taobao.com>
2017-10-24 17:35:01 +0800

    feat: add files

---
+++ start.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+cd /tmp/falcon-plus/scripts/mysql/db_schema/
+mysql -h 127.0.0.1 -u root -p < 1_uic-db-schema.sql
+mysql -h 127.0.0.1 -u root -p < 2_portal-db-schema.sql
+mysql -h 127.0.0.1 -u root -p < 3_dashboard-db-schema.sql
+mysql -h 127.0.0.1 -u root -p < 4_graph-db-schema.sql
+mysql -h 127.0.0.1 -u root -p < 5_alarms-db-schema.sql
+
+cd $WORKSPACE
+./open-falcon start
